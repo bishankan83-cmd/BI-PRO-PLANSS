8936e4b9dd89087195cd24afe28359c741822d55
Bishan Kanthana <bishankan83@gmail.com>
2026-05-07 12:41:42 +0530

    yyy
    yyy

--- dynamic/includes/footer.php
+++
@@ -1,6 +0,0 @@
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; BiTech Systems Solutions <?php echo date('Y');?></p>
-      </div>
-      <!-- /.container -->
-    </footer>
